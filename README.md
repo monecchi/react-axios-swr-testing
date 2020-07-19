3de5e2f0e84cc000fce9e504c2f2692765b0c171
monecchi <fornacciari@gmail.com>
2020-07-19 13:47:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-axios-swr-testing
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-axios-swr-testing)
---
+++ index.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import "./style.css";
+import axios from "axios";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stores: null,
+      loading: false
+    };
+    //this.getRestaurants.bind(this);
+  }
+
+  getRestaurants() {
+    this.setState({ loading: true });
+    return axios
+      .get("https://pizzariameurancho.com.br/wp-json/mrp/v1/stores")
+      .then(response => {
+        console.log(response);
+        this.setState({
+          stores: response.data,
+          loading: false
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  componentDidMount() {
+    this.getRestaurants();
+  }
+
+  render() {
+    const { stores, loading } = this.state;
+    return (
+      <div>
+        <h1>swr</h1>
+        <p>
+          <code>Testing: mutate()</code>
+        </p>
+        <button
+          onClick={e => {
+            e.preventDefault(), this.getRestaurants();
+          }}
+        >
+          Get data
+        </button>
+        <div style={{ paddingTop: "1rem" }}>
+          {stores &&
+            stores.map(store => {
+              return (
+                <p>{store.slug}</p>
+              );
+            })}
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
