6795f1a9fd1c2fe79e14f34c129f5c9554eecfa4
monecchi <fornacciari@gmail.com>
2020-07-20 03:45:16 -0300

    1 file modified

--- fetchRestaurantData.js
+++ fetchRestaurantData.js
@@ -2,17 +2,17 @@ import React, { Component } from "react";
 import useSWR from "swr";
 import axios from "axios";
 
-// Set up swr
+// Setup SWR
 
-const fetcher = (...args) => fetch(...args).then(res => res.json()); // default fetcher with fetch
-// const fetcher = url => fetch(url).then(res => res.json()); // with fetch api
+// define a "fetcher" ( wrapper of the native fetch )
+const fetcher = (...args) => fetch(...args).then(res => res.json()); // with fetch
+// const fetcher = url => fetch(url).then(res => res.json()); // same as above
 // const fetcher = url => axios.get(url).then(res => res.data); // with axios
 
 let apiURL = "https://pizzariameurancho.com.br/wp-json/mrp/v1/stores/";
 
 // Get a single restaurant (store) data
 export const getStore = slug => {
-  
   const { data, error } = useSWR(apiURL + `${slug}`, fetcher);
 
   return {
@@ -23,4 +23,12 @@ export const getStore = slug => {
 };
 
 // Get all restaurants (stores) data
-export const getStores = () => {};
+export const getStores = () => {
+  const { data, error } = useSWR(apiURL, fetcher);
+
+  return {
+    stores: data,
+    isLoading: !error && !data,
+    isError: error
+  };
+};
