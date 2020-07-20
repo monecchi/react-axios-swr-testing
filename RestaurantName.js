838369bc35cab140c60317dc6793fa7acff45ad4
monecchi <fornacciari@gmail.com>
2020-07-20 02:52:36 -0300

    1 file modified

--- RestaurantName.js
+++ RestaurantName.js
@@ -5,7 +5,7 @@ const RestaurantName = ({slug}) => {
   const { store, isLoading } = getStore(slug);
   if (isLoading) return <div><p>Loading...</p></div>;
   console.log(store);
-  return <h1 className={}>Loja {store}</h1>;
+  return <h1 className="">Loja {store}</h1>;
 };
 
 export default RestaurantName;
